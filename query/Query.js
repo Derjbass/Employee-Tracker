9c58251887e32a9ed178b951730c10cf7fac4f74
Derjbass <Derek.j.bass@gmail.com>
2022-07-28 18:07:12 -0500

    added connection/Query files

---
+++ query/Query.js
@@ -0,0 +1,24 @@
+class Query{
+    constructor(id){
+        this.id = id;
+    }
+
+    // function to get information from db
+    getQuery() {
+
+    };
+
+    // function to add data to db
+    postQuery() {
+
+    };
+
+    // function to update data currently in db
+    updateQuery() {
+
+    }
+}
+
+
+// export
+module.exports = Query;
---
+++ server.js
@@ -0,0 +1,4 @@
+//imports
+const inq = require('inquirer');
+const q = require('Query');
+const query = new q;
